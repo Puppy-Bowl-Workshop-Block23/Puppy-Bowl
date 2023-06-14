fbd129a95a4295cd3e9ea149b0502019ba7f4cf8
Maria Campos <campos.maria04@outlook.com>
2023-06-14 18:19:56 -0500

    styling player cards to show up in grid form

--- script.js
+++ script.js
@@ -121,10 +121,13 @@ const removePlayer = async (playerId) => {
 const renderAllPlayers = (playerList) => {
     try {
         const players = fetchAllPlayers();
+
         players.innerHTML = '';
+        //for each player card loop
         playerList.forEach((player) => {
             const puppyElement = document.createElement('div')
             puppyElement.classList.add('player');
+
             puppyElement.innerHTML = `
             <h2>${player.name}</h2>
             <p> Breed: ${player.breed}</p>
@@ -139,6 +142,7 @@ const renderAllPlayers = (playerList) => {
             `;
 
             playerContainer.appendChild(puppyElement);
+        
         });
     }
 
